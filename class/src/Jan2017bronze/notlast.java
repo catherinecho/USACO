9f1e4042b876ede49d9a61eaa544777b5064b0ad
Catherine <ccho20112@gmail.com>
2019-10-18 17:10:56 -0700

    solved notlast (from usaco)

--- class/src/Jan2017bronze/notlast.java
+++ class/src/Jan2017bronze/notlast.java
@@ -7,6 +7,7 @@ public class notlast {
 	static PrintWriter out;
 	static int n; 
 	static Map<String,Integer> list;
+	static cows [] c;
 	public static void main(String[] args)throws IOException {
 		in = new Scanner(new File("notlast.in"));
 		out = new PrintWriter(new File("notlast.out"));
@@ -33,11 +34,40 @@ public class notlast {
 				list.put(name,num);
 			}
 		}
-		System.out.println(list);
+		//System.out.println(list);
+		c = new cows[list.size()];
+		int a = 0;
+		for( Map.Entry<String,Integer> list: list.entrySet()) {
+			c[a] = new cows(list.getKey(),list.getValue());
+			a++;
+		}
+		Arrays.sort(c);
+		/*
+		for(int i = 0; i < list.size(); i++) {
+			System.out.println(c[i].name + " " + c[i].milk);
+		}
+		*/
 		
 	}
-	public static int solve() {
-		return 0;
+	public static String solve() {
+		boolean t = false;
+		int first = c[0].milk;
+		for(int i = 0; i < list.size(); i++) {
+			if(c[i].milk != first) {
+				t = true;
+			}
+		}
+		if(t != true && list.size()!= 1) {
+			return "Tie";
+		}
+		for(int i = 0 ; i < list.size(); i++) {
+			if(c[i].milk != first) {
+				return c[i].name;
+				
+			}
+		}
+		
+		return c[0].name;
 	}
 }
 class cows implements Comparable<cows>{
@@ -45,8 +75,7 @@ class cows implements Comparable<cows>{
 	public int milk;
 	public int compareTo(cows m) {
 		return this.milk - m.milk;
-		
-		
+
 	}
 	public cows(String n, int m) {
 		this.name = n;
